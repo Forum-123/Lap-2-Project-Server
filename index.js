dea7df5f262c3cfe6403f0614ac436030b672643
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-10-25 18:19:53 +0100

    Move index.js for Heroku

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = require("./server");
+const app = require("./api/server");
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Express now departing from port ${port}!`))
