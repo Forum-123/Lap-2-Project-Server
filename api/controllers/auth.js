8773143504686e7a136de2f15b78835b50e4c873
“nxforg” <christopher.anstead@yahoo.co.uk>
2021-10-25 15:52:55 +0100

    add token generation on login

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv').config();
 const router = express.Router();
 
 const User = require('../models/user');
@@ -14,4 +15,30 @@ router.post('/register', async (req, res) => {
     } catch (err) {
         res.status(500).json({err});
     }
-})
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findByEmail(req.body.email)
+        if(!user){ throw new Error('No user with this email') }
+        const authed = await bcrypt.compare(req.body.password, user.password)
+        if (authed){
+            const sendToken = (err, token) => {
+                if(err){ throw new Error('Could not create token')}
+                res.status(200).json({
+                    success: true,
+                    token: 'Bearer ' + token
+                })
+            } 
+            const secret = process.env.TOKEN_SECRET;
+            const payload = { user: user.name, email:user.email }
+            jwt.sign(payload, secret, { expiresIn: '2h' }, sendToken)
+        } else {
+            throw new Error('User could not be authenticated')  
+        }
+    } catch (err) {
+        res.status(401).json({ err });
+    }
+});
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -5,6 +5,10 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+const authRoutes = require('./controllers/auth');
+
+server.use('/auth', authRoutes);
+
 server.get('/', (req, res) => res.send('Welcome to Habit Tracker'));
 
 module.exports = server;
