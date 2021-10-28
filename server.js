2a38e87167ba5a8fa18777f90e9d9132c8fcf766
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-10-28 17:45:06 +0100

    Update code for deployment

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const cors = require("cors");
 const server = express();
 server.use(cors());
 server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
-
 
 const authRoutes = require('./routes/users');
 const habitsRoutes = require('./routes/habits');
